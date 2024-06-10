ce7ded865325746a1a5ef2a25b8c50b6a6206165
animesia-app <aax955600@gmail.com>
2024-06-10 16:08:01 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add event listener to animate text on scroll
+window.addEventListener('scroll', function() {
+  const animatedText = document.querySelector('.animated-text');
+  animatedText.classList.add('animate');
+});
